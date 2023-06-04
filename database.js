d06226cee8ceaefbf5d551d3f57138d6ee159a5a
jackiechanmakes <jackiechanmakes@gmail.com>
2023-06-03 22:19:19 -0700

    Add MySQL database access from api server

--- database.js
+++ database.js
@@ -1,6 +1,9 @@
 // Get the client
 import mysql from 'mysql2';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 // Create the connection to database
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
@@ -9,10 +12,70 @@ const pool = mysql.createPool({
     database: process.env.MYSQL_DATABASE
   }).promise();
 
-async function getEvents() {
+export async function getEvents() {
     const [rows] = await pool.query("SELECT * FROM events");
     return rows;
 }
 
-const events = await getEvents();
-console.log(events);
+export async function getEvent(id) {
+  const [rows] = await pool.query(`
+  SELECT *
+  FROM events
+  WHERE id = ?`, [id]);
+  return rows[0];
+}
+
+export async function getEventsByDate(date) {
+  const [rows] = await pool.query(`
+  SELECT *
+  FROM events
+  WHERE date LIKE ?
+  `, [date]
+  );
+
+  return rows;
+}
+
+export async function createEvent(date, title) {
+  const [result] = await pool.query(`
+  INSERT INTO events (date, title)
+  VALUES (?, ?)
+  `, [date, title]);
+  const id = result.insertId;
+  return getEvent(id);
+}
+
+export async function deleteEvent(id) {
+  await pool.query(`
+  DELETE FROM events
+  WHERE id = ?
+  `, [id]);
+}
+
+export async function updateEvent(title, id) {
+  const [result] = await pool.query(`
+  UPDATE events
+  SET title = ?
+  WHERE id = ?
+  `, [title, id]);
+  return getEvent(id);
+}
+
+export async function replaceEvent(idParam, id, date, title) {
+  await pool.query(`
+  UPDATE events
+  SET id = ?, date = ?, title = ?
+  WHERE id = ?
+  `, [id, date, title, idParam]);
+  return getEvent(id);
+}
+
+export async function getStats() {
+  let result = await pool.query(`
+  SELECT date, COUNT(*) as count
+  FROM events
+  GROUP BY date
+  ORDER BY date ASC
+  `);
+  return result[0];
+}
--- index.js
+++ index.js
@@ -1,94 +1,62 @@
-const express = require('express');
+
+import express from 'express'
+import { getEvents, getEventsByDate, createEvent, deleteEvent, updateEvent, replaceEvent, getStats } from './database.js'
+
 const app = express();
 app.use(express.json());
 
-let events = [
-  { id: 1, title: 'My family', date: '2023-05-10' },
-  { id: 2, title: 'Good health', date: '2023-04-10' },
-  { id: 3, title: 'Healthy food', date: '2023-04-15' },
-  { id: 4, title: 'Dogs', date: '2023-04-23' },
-  { id: 5, title: 'Cats', date: '2023-04-30' },
-  { id: 6, title: 'Medicine', date: '2023-04-30' },
-  { id: 7, title: 'Therapy', date: '2023-05-20' },
-  { id: 8, title: 'Music', date: '2023-05-30'},
-  { id: 9, title: 'Arts and Crafts', date: '2023-05-08' },
-  { id: 10 ,title: 'Sense of sight', date: '2023-05-09' }
-];
-
-const generateId = () => {
-  const maxId = events.length > 0
-    ? Math.max(...events.map(element => element.id))
-    : 0;
-  return maxId + 1;
-}
-
-app.get('/events', (request, response) => {
+app.get('/events', async (request, response) => {
+  let events;
   let date = request.query.date;
   if (date === undefined) {
-    response.json(events);
+    events = await getEvents();
   } else {
-    const data = events.filter(event => event.date === date);
-    if (data.length > 0) {
-      response.json(data);
-    } else {
-      response.status(404).send("Events logged corresponding to that date not found");
-    }
+    events = await getEventsByDate(date);
+  }
+
+  if (events.length > 0) {
+    response.send(events);
+  } else {
+    response.status(404).send("Events logged corresponding to that date not found");
   }
 });
 
-app.delete('/events/:id', (request, response) => {
+app.delete('/events/:id', async (request, response) => {
   const id = parseInt(request.params.id);
-  events = events.filter(event => event.id !== id);
+  await deleteEvent(id);
   response.status(204).end();
 });
 
-app.post('/events', (request, response) => {
-  const body = request.body;
+app.post('/events', async (request, response) => {
+  const {date, title} = request.body;
 
-  if (!body.title) {
+  if (!title) {
     return response.status(400).json({
       error: 'Content missing'
     });
   }
 
-  const event = {
-    id: generateId(),
-    title: body.title,
-    date: body.date
-  }
-
-  events = events.concat(event);
-  response.json(event);
+  let event = await createEvent(date, title);
+  response.status(201).send(event);
 });
 
-app.put('/events/:id', (request, response) => {
-  const event = events.find(element => element.id === parseInt(request.params.id));
-  const index = events.indexOf(event);
-  events[index] = request.body;
-  response.json(events[index]);
+app.put('/events/:id', async (request, response) => {
+  let idParam = request.params.id;
+  const {id, date, title} = request.body;
+  let event = await replaceEvent(idParam, id, date, title);
+  response.send(event);
 });
 
-app.patch('/events/:id', (request, response) => {
-  const event = events.find(element => element.id === parseInt(request.params.id));
-  const index = events.indexOf(event);
-  events[index].title = request.body.title;
-  response.json(events[index]);
+app.patch('/events/:id', async (request, response) => {
+  let id = parseInt(request.params.id);
+  let title = request.body.title;
+  let event = await updateEvent(title, id);
+  response.send(event);
 });
 
-app.get('/stats', (request, response) => {
-  let count = 0;
-  let stats = {};
-
-  for (let i = 0; i < events.length; i++) {
-    let date = events[i].date;
-    if (date in stats) {
-      ++stats[date];
-    } else {
-      stats[date] = 1;
-    }
-    count++;
-  }
-  response.json(stats);
+app.get('/stats', async (request, response) => {
+  let stats = await getStats();
+  response.send(stats);
 });
 
 const PORT = 3005;
